68daa6f2b711f5625a3bbfa1463d8085ecd4e094
mwurch <wurch.marcel@gmail.com>
2024-03-01 20:48:12 +0100

    cpp 01 ex06 done

---
+++ cpp01/ex06/Harl.cpp
@@ -0,0 +1,51 @@
+#include <cstddef>
+#include "Harl.hpp"
+
+Harl::Harl() {}
+Harl::~Harl() {}
+
+void Harl::debug()
+{
+    std::cout << "\033[32m";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << "\033[0m";
+
+}
+
+void Harl::info()
+{
+    std::cout << "\033[33m";
+    std::cout << "I cannot believe adding extra bacon costs more money. You didnt put enough bacon in my burger! If you did, I wouldnt be asking for more!" << std::endl;
+    std::cout << "\033[0m";
+}
+
+void Harl::warning()
+{
+    std::cout << "\033[34m";
+    std::cout << "I think I deserve to have some extra bacon for free. Ive been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "\033[0m";
+}
+
+void Harl::error()
+{
+    std::cout << "\033[35m";
+    std::cout << "This is unacceptable! I want to speak to the manager now" << std::endl;
+    std::cout << "\033[0m";
+}
+
+void Harl::complain(std::string level)
+{
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+    void    (Harl::*functionPtr[])(void) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+            (this->*functionPtr[i])();
+    }
+}
---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include"Harl.hpp"
+
+int main(int argc, char **argv)
+{
+    int i;
+    Harl obj;
+    std::string input = argv[1];
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+
+    if (argc != 2)
+        return (-1);
+    for (i = 0; i < 4; i++)
+    {
+        if (input == levels[i])
+            break ;
+    }
+    switch (i) {
+
+    case 0:
+        obj.complain("DEBUG");
+        // Fall through intentionally
+    case 1:
+        obj.complain("INFO");
+        // Fall through intentionally
+    case 2:
+        obj.complain("WARNING");
+        // Fall through intentionally
+    case 3:
+        obj.complain("ERROR");
+        break ;
+    default:
+        std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
+} 
+
+    return (0);
+}
